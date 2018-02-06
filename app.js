a0bfb4eb8a51c51443cbedd02d68eabd7db970fc
Prabodh <prabodhdasari@gmail.com>
2018-02-06 11:40:04 +0530

    changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var x = require("http");
 
 console.log("Hello world");
-<<<<<<< HEAD
+ 
 console.log("test");
-=======
+ 
 var x = require("http");
 
 var x = require("http");
@@ -11,4 +11,4 @@ var x = require("http");
 var x = require("http");
 var x = require("http");
 
->>>>>>> 3298fd2f1888a3587d0ec5ad809416a7823adead
+ 
